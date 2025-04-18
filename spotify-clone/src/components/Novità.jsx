9a86769ded05ecd97cfcf84608da7d9e0062d42a
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-04-18 15:29:38 +0200

    Update Novità.jsx

--- spotify-clone/src/components/Novità.jsx
+++ spotify-clone/src/components/Novità.jsx
@@ -5,23 +5,24 @@ import "../scrollImages.css";
 
 function News() {
   return (
-    <Container fluid className=''>
-      <Row className='d-flex'>
-        <Col className=''>
-          <h1>Novità</h1>
+    <Container fluid>
+      <Row>
+        <Col>
+          <h1 className='text-start'>Novità</h1>
           <hr />
         </Col>
       </Row>
-      <Row className=''>
-        <Col className=''>
+
+      <Row className='justify-content-center'>
+        <Col md={5} className='text-start mb-4'>
           <h5 className='fs-6 text-secondary'>NUOVA STAZIONE RADIO</h5>
           <h3>Rilassati, al resto pensiamo noi. Ascolta Apple Music Chill</h3>
-          <img src={musicChill} alt='Apple Music Chill' width='500' />
+          <img src={musicChill} alt='Apple Music Chill' width='100%' />
         </Col>
-        <Col className=''>
+        <Col md={5} className='text-start mb-4'>
           <h5 className='fs-6 text-secondary'>NUOVA STAZIONE RADIO</h5>
           <h3>Ecco la nuova casa della musica latina</h3>
-          <img src={musicLatin} alt='Musica Latina' width='500' />
+          <img src={musicLatin} alt='Musica Latina' width='100%' />
         </Col>
       </Row>
     </Container>
